293bc2a308c4f5f5c230644b80bfe30b1d30c0c1
Maradona Morais <maradona.morais@hotmail.com>
2020-03-12 12:12:19 -0300

    improving tokenizer performance

--- postag/utils/evaluator.py
+++ postag/utils/evaluator.py
@@ -6,15 +6,20 @@ def eval(tokens):
     tbank = treebank.Treebank()
 
     for token in tokens:
-        if len(level_stack) == 0 and token[0] == TokenType.OPEN_PR:
-            level_stack.append(tbank.new_instance())
+        if len(level_stack) == 0 and token[0] == 'open_pr':
+            continue
+            # level_stack.append(tbank.new_instance())
         else:
-            if token[0] == TokenType.OPEN_PR:
-                level_stack.append(level_stack[-1].new_child())
-            if token[0] == TokenType.ENT_NAME:
-                level_stack[-1].class_name = token[1]
-            if token[0] == TokenType.ENT_VALUE:
-                level_stack[-1].value = token[1]
-            if token[0] == TokenType.CLOSE_PR:
-                level_stack.pop()
+            if token[0] == 'open_pr':
+                continue
+                # level_stack.append(level_stack[-1].new_child())
+            if token[0] == 'close_pr':
+                continue
+                # level_stack.pop()
+            if token[0] == 'ent_name':
+                continue
+                # level_stack[-1].class_name = token[1]
+            if token[0] == 'ent_val':
+                continue
+                # level_stack[-1].value = token[1]
     return tbank
--- postag/utils/reader.py
+++ postag/utils/reader.py
@@ -5,7 +5,8 @@ import re
 import os
 
 from ..core import treebank
-from .tokenizer import Tokenizer
+# from .tokenizer import Tokenizer
+from .tokken import tokenizer
 from .evaluator import eval
 
 class PTBReader:
@@ -15,13 +16,11 @@ class PTBReader:
         self.path = path
 
     def read(self):
-        tokenizer = Tokenizer()
+        ptbank = None
 
         with open(self.path, "r") as file:
-            f_read_ch = functools.partial(file.read, 1)
-
-            for ch in iter(f_read_ch, ''):
-                tokenizer.digest_char(ch)
-
-        ptbank = eval(tokenizer.get_tokens())
+            f_read_chunk = functools.partial(file.read, 1000)
+            ptbank = tokenizer(iter(f_read_chunk, ''))
+            print(len(ptbank.instances))
+            print(len(ptbank.instances[0].children))
         return ptbank
